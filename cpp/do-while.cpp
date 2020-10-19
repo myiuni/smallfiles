2c017d4dc1f31758f5bb1dc5d7eee3a8482b7e39
myiuni <myiuni@163.com>
2020-10-19 20:33:19 +0800

    This is do-while

---
+++ cpp/do-while.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+    > File Name: do-while.cpp
+    > Author: myiuni
+    > Mail: 1792526255@qq.com 
+    > Created Time: 2020年10月19日 星期一 20时30分59秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 0;
+
+	do
+	{
+		cout << num << endl;
+		num++;
+	}
+	while (num < 10);
+}
